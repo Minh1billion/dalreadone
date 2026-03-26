81873eb4f1da36dc82f59dab22071f3b64032be2
Minh1billion <minh01212929979@gmail.com>
2026-03-26 19:19:33 +0700

    feat: Add startup event to initialize database schema on application startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
 from app.routers import auth, files, projects, query, oauth
+from app.db.session import engine
+from app.models import Base
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 # Oauth routes
 app.include_router(oauth.router)
@@ -12,7 +17,6 @@ app.include_router(auth.router, prefix="/api")
 app.include_router(projects.router, prefix="/api")
 app.include_router(files.router, prefix="/api")
 app.include_router(query.router, prefix="/api")
-
 
 @app.get("/")
 def root():
